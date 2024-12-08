27a5e515bfc793f25a69da2541d828a9d9e9bf38
hdy510 <hdy510@naver.com>
2024-12-08 16:29:21 +0900

    241208 responsive

--- portfolio-page/js.js
+++ portfolio-page/js.js
@@ -6,7 +6,14 @@ $(document).ready(function () {
         damping: 0.1,
         renderByPixels: true,
     });
+    // 스크롤 이벤트 리스너 추가
+    scrollbarInstance.addListener(updateActiveMenu);
 
+
+
+
+
+    
 
     // 메뉴 클릭 여부를 추적하는 플래그
     let isMenuClicked = false;
@@ -19,10 +26,8 @@ $(document).ready(function () {
             }, 500);
             return;
         }
-
         const scrollTop = scrollbarInstance.scrollTop;
         const $menu = $('header .menu li');
-        
         // 각 섹션의 위치 정보 확인
         const sections = [
             { element: document.querySelector('main'), index: 0 },
@@ -30,33 +35,26 @@ $(document).ready(function () {
             { element: document.querySelector('.portfolio'), index: 2 },
             { element: document.querySelector('.contact'), index: 3 }
         ];
-        
         // 각 섹션의 위치를 확인하여 적절한 메뉴 활성화
         sections.forEach(section => {
             const sectionTop = section.element.offsetTop;
             const sectionHeight = section.element.offsetHeight;
-            
             if (scrollTop >= sectionTop - 200 && scrollTop < sectionTop + sectionHeight - 200) {
                 $menu.eq(section.index).addClass('on').siblings().removeClass('on');
             }
         });
     }
-    // 스크롤 이벤트 리스너 추가
-    scrollbarInstance.addListener(updateActiveMenu);
+    
 
 
 
     // header 의 menu 클릭 설정
     $('header .menu li').click(function () {
         let $i = $(this).index();
-        
         // 메뉴 클릭 플래그 설정
         isMenuClicked = true;
-
-
         // 1. list 모양 변경
         $(this).addClass('on').siblings().removeClass('on');
-        
         // 2. 메뉴 클릭 시 해당 항목으로 스크롤 이동
         let targetElement;
         switch($i) {
@@ -81,17 +79,14 @@ $(document).ready(function () {
                 duration: 500
             });
         }
-
-
-        
     });
+
 
 
     // portfolio 의 list 클릭 설정
     $('.portfolio .list li').click(function () {
         // 1. list 모양 변경
         $(this).addClass('on').siblings().removeClass('on');
-
         // 2. 컨텐츠 변경
         if ($(this).index() == 0) {
             // 2024 작업물 보여주기
@@ -102,7 +97,6 @@ $(document).ready(function () {
             $('.work2025').show().css({'display': 'flex'});
             $('.work2024').hide();
         }
-        
     });
 
 
@@ -112,12 +106,10 @@ $(document).ready(function () {
         $('.portfolio .btnBox button').click(function () {
             // 현재 클릭된 li 요소
             let $currentItem = $(this).parent().parent().parent();
-            
             // work2024 클래스를 가진 li에서 고유한 브랜드/프로젝트 클래스 추출
             let projectNameClass = $currentItem.attr('class').split(' ').find(cls => 
                 cls !== 'contentsBox' && cls !== 'work2024'
             );
-    
             // 해당 브랜드/프로젝트 클래스를 가진 이미지에 'on' 클래스 추가
             $('.conceptDesign .imgBox img').removeClass('on');
             $('.conceptDesign .imgBox img').each(function() {
@@ -125,16 +117,19 @@ $(document).ready(function () {
                     $(this).addClass('on');
                 }
             });
-    
+            // 컨텐츠 등장
             $('.conceptDesign').show().css({'display': 'flex'});
         });
     }
     handlePortfolioClick();
-
     // concept design 이미지 퇴장 설정
     $('.conceptDesign button').click(function () {
         $('.conceptDesign').hide();
         $('.conceptDesign .imgBox img').removeClass('on');
+    });
+    // concept design 이미지 스크롤 맨 위로 이동
+    $('.portfolio .btnBox button').click(function () {
+        $('.conceptDesign .imgBox').scrollTop(0);
     });
 
 
@@ -143,7 +138,6 @@ $(document).ready(function () {
     // 메일 주소 복사 기능
     $('.contact .mail .copy').click(function () {
         let $copyText = $('.contact .mail .link b').text();
-        
         // Clipboard API 사용
         navigator.clipboard.writeText($copyText).then(function() {
             // 복사 알림 메시지
@@ -166,7 +160,6 @@ $(document).ready(function () {
                 // 애니메이션 완료 후 display 초기화
                 $(this).css('display', 'none');
             });
-
         }).catch(function(err) {
             // 복사 실패 시 에러 처리
             console.error('복사 중 오류 발생:', err);
